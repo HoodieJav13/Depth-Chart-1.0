566460a986f8c2244519086f059e4c189d919a60
HoodieJav13 <jav13chav32@gmail.com>
2026-08-05 22:44:12 -0600

    Remove browser-side coach allowlist

--- src/auth/approvedCoaches.ts
+++
@@ -1,4 +0,0 @@
-const APPROVED_COACH_NUMBERS = new Set(["+15057307634"]);
-
-export const isApprovedCoach = (phoneNumber: string | null): boolean =>
-  phoneNumber !== null && APPROVED_COACH_NUMBERS.has(phoneNumber);
